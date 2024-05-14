e6052312459b46924d1abf49a985cfa5c63727e6
eihaku <eihaku0205@gmail.com>
2024-05-14 14:33:55 +0900

    botan新規作成

---
+++ botan1/Form1.cs
@@ -0,0 +1,459 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace botan1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(button1.BackColor == Color.Red)
+            { 
+                button1.BackColor = Color.Green;
+                button1.Text = "ON";
+            }
+            else
+            {
+                button1.BackColor = Color.Red;
+                button1.Text = "OFF";
+            }
+        }
+
+        private void button65_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.BackColor == Color.Red)
+            {
+                button2.BackColor = Color.Green;
+                button2.Text = "ON";
+            }
+            else
+            {
+                button2.BackColor = Color.Red;
+                button2.Text = "OFF";
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.BackColor == Color.Red)
+            {
+                button3.BackColor = Color.Green;
+                button3.Text = "ON";
+            }
+            else
+            {
+                button3.BackColor = Color.Red;
+                button3.Text = "OFF";
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.BackColor == Color.Red)
+            {
+                button5.BackColor = Color.Green;
+                button5.Text = "ON";
+            }
+            else
+            {
+                button5.BackColor = Color.Red;
+                button5.Text = "OFF";
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.BackColor == Color.Red)
+            {
+                button4.BackColor = Color.Green;
+                button4.Text = "ON";
+            }
+            else
+            {
+                button4.BackColor = Color.Red;
+                button4.Text = "OFF";
+            }
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (button7.BackColor == Color.Red)
+            {
+                button7.BackColor = Color.Green;
+                button7.Text = "ON";
+            }
+            else
+            {
+                button7.BackColor = Color.Red;
+                button7.Text = "OFF";
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button6.BackColor == Color.Red)
+            {
+                button6.BackColor = Color.Green;
+                button6.Text = "ON";
+            }
+            else
+            {
+                button6.BackColor = Color.Red;
+                button6.Text = "OFF";
+            }
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (button9.BackColor == Color.Red)
+            {
+                button9.BackColor = Color.Green;
+                button9.Text = "ON";
+            }
+            else
+            {
+                button9.BackColor = Color.Red;
+                button9.Text = "OFF";
+            }
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (button8.BackColor == Color.Red)
+            {
+                button8.BackColor = Color.Green;
+                button8.Text = "ON";
+            }
+            else
+            {
+                button8.BackColor = Color.Red;
+                button8.Text = "OFF";
+            }
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (button11.BackColor == Color.Red)
+            {
+                button11.BackColor = Color.Green;
+                button11.Text = "ON";
+            }
+            else
+            {
+                button11.BackColor = Color.Red;
+                button11.Text = "OFF";
+            }
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (button10.BackColor == Color.Red)
+            {
+                button10.BackColor = Color.Green;
+                button10.Text = "ON";
+            }
+            else
+            {
+                button10.BackColor = Color.Red;
+                button10.Text = "OFF";
+            }
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (button13.BackColor == Color.Red)
+            {
+                button13.BackColor = Color.Green;
+                button13.Text = "ON";
+            }
+            else
+            {
+                button13.BackColor = Color.Red;
+                button13.Text = "OFF";
+            }
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (button12.BackColor == Color.Red)
+            {
+                button12.BackColor = Color.Green;
+                button12.Text = "ON";
+            }
+            else
+            {
+                button12.BackColor = Color.Red;
+                button12.Text = "OFF";
+            }
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (button14.BackColor == Color.Red)
+            {
+                button14.BackColor = Color.Green;
+                button14.Text = "ON";
+            }
+            else
+            {
+                button14.BackColor = Color.Red;
+                button14.Text = "OFF";
+            }
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if (button16.BackColor == Color.Red)
+            {
+                button16.BackColor = Color.Green;
+                button16.Text = "ON";
+            }
+            else
+            {
+                button16.BackColor = Color.Red;
+                button16.Text = "OFF";
+            }
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (button15.BackColor == Color.Red)
+            {
+                button15.BackColor = Color.Green;
+                button15.Text = "ON";
+            }
+            else
+            {
+                button15.BackColor = Color.Red;
+                button15.Text = "OFF";
+            }
+
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            if (button19.BackColor == Color.Red)
+            {
+                button19.BackColor = Color.Green;
+                button19.Text = "ON";
+            }
+            else
+            {
+                button19.BackColor = Color.Red;
+                button19.Text = "OFF";
+            }
+
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            if (button20.BackColor == Color.Red)
+            {
+                button20.BackColor = Color.Green;
+                button20.Text = "ON";
+            }
+            else
+            {
+                button20.BackColor = Color.Red;
+                button20.Text = "OFF";
+            }
+
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            if (button18.BackColor == Color.Red)
+            {
+                button18.BackColor = Color.Green;
+                button18.Text = "ON";
+            }
+            else
+            {
+                button18.BackColor = Color.Red;
+                button18.Text = "OFF";
+            }
+
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (button17.BackColor == Color.Red)
+            {
+                button17.BackColor = Color.Green;
+                button17.Text = "ON";
+            }
+            else
+            {
+                button17.BackColor = Color.Red;
+                button17.Text = "OFF";
+            }
+
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            if (button22.BackColor == Color.Red)
+            {
+                button22.BackColor = Color.Green;
+                button22.Text = "ON";
+            }
+            else
+            {
+                button22.BackColor = Color.Red;
+                button22.Text = "OFF";
+            }
+
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            if (button21.BackColor == Color.Red)
+            {
+                button21.BackColor = Color.Green;
+                button21.Text = "ON";
+            }
+            else
+            {
+                button21.BackColor = Color.Red;
+                button21.Text = "OFF";
+            }
+
+        }
+
+        private void button26_Click(object sender, EventArgs e)
+        {
+            if (button26.BackColor == Color.Red)
+            {
+                button26.BackColor = Color.Green;
+                button26.Text = "ON";
+            }
+            else
+            {
+                button26.BackColor = Color.Red;
+                button26.Text = "OFF";
+            }
+
+        }
+
+        private void button27_Click(object sender, EventArgs e)
+        {
+            if (button27.BackColor == Color.Red)
+            {
+                button27.BackColor = Color.Green;
+                button27.Text = "ON";
+            }
+            else
+            {
+                button27.BackColor = Color.Red;
+                button27.Text = "OFF";
+            }
+
+        }
+
+        private void button32_Click(object sender, EventArgs e)
+        {
+            if (button32.BackColor == Color.Red)
+            {
+                button32.BackColor = Color.Green;
+                button32.Text = "ON";
+            }
+            else
+            {
+                button32.BackColor = Color.Red;
+                button32.Text = "OFF";
+            }
+
+        }
+
+        private void button33_Click(object sender, EventArgs e)
+        {
+            if (button33.BackColor == Color.Red)
+            {
+                button33.BackColor = Color.Green;
+                button33.Text = "ON";
+            }
+            else
+            {
+                button33.BackColor = Color.Red;
+                button33.Text = "OFF";
+            }
+
+        }
+
+        private void button31_Click(object sender, EventArgs e)
+        {
+            if (button31.BackColor == Color.Red)
+            {
+                button31.BackColor = Color.Green;
+                button31.Text = "ON";
+            }
+            else
+            {
+                button31.BackColor = Color.Red;
+                button31.Text = "OFF";
+            }
+
+        }
+
+        private void button29_Click(object sender, EventArgs e)
+        {
+            if (button29.BackColor == Color.Red)
+            {
+                button29.BackColor = Color.Green;
+                button29.Text = "ON";
+            }
+            else
+            {
+                button29.BackColor = Color.Red;
+                button29.Text = "OFF";
+            }
+
+        }
+
+        private void button36_Click(object sender, EventArgs e)
+        {
+            if (button36.BackColor == Color.Red)
+            {
+                button36.BackColor = Color.Green;
+                button36.Text = "ON";
+            }
+            else
+            {
+                button36.BackColor = Color.Red;
+                button36.Text = "OFF";
+            }
+
+        }
+    }
+}
